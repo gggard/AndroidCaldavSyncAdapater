a209986c41b2d916f7445c6b0ee196acb93c52a9
gege <gege@gege.org>
2013-02-19 13:54:38 +0100

    Intermediate

--- CalDAVSyncAdapter/gen/org/gege/caldavsyncadapter/R.java
+++ CalDAVSyncAdapter/gen/org/gege/caldavsyncadapter/R.java
@@ -8,82 +8,53 @@
 package org.gege.caldavsyncadapter;
 
 public final class R {
-    public static final class array {
-        public static final int pref_example_list_titles=0x7f060000;
-        public static final int pref_example_list_values=0x7f060001;
-        public static final int pref_sync_frequency_titles=0x7f060002;
-        public static final int pref_sync_frequency_values=0x7f060003;
-    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int icon=0x7f020001;
-        public static final int icon_old=0x7f020002;
     }
     public static final class id {
-        public static final int login=0x7f090005;
-        public static final int login_form=0x7f090002;
-        public static final int login_status=0x7f090000;
-        public static final int login_status_message=0x7f090001;
-        public static final int menu_forgot_password=0x7f090008;
-        public static final int password=0x7f090004;
-        public static final int sign_in_button=0x7f090007;
-        public static final int url=0x7f090006;
-        public static final int user=0x7f090003;
+        public static final int login=0x7f080005;
+        public static final int login_form=0x7f080002;
+        public static final int login_status=0x7f080000;
+        public static final int login_status_message=0x7f080001;
+        public static final int menu_forgot_password=0x7f080008;
+        public static final int password=0x7f080004;
+        public static final int sign_in_button=0x7f080007;
+        public static final int url=0x7f080006;
+        public static final int user=0x7f080003;
     }
     public static final class layout {
         public static final int activity_authenticator=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_authenticator=0x7f080000;
+        public static final int activity_authenticator=0x7f070000;
     }
     public static final class string {
-        public static final int action_sign_in_register=0x7f050007;
-        public static final int action_sign_in_short=0x7f050008;
+        public static final int action_sign_in_register=0x7f050005;
+        public static final int action_sign_in_short=0x7f050006;
         public static final int app_name=0x7f050000;
-        public static final int error_connection_refused=0x7f050013;
-        public static final int error_field_required=0x7f05000e;
-        public static final int error_incorrect_password=0x7f05000d;
-        public static final int error_incorrect_url_format=0x7f050010;
-        public static final int error_invalid_email=0x7f05000b;
-        public static final int error_invalid_password=0x7f05000c;
-        public static final int error_invalid_server_answer=0x7f050011;
-        public static final int error_unkown_error=0x7f050014;
-        public static final int error_wrong_url=0x7f050012;
+        public static final int error_connection_refused=0x7f050011;
+        public static final int error_field_required=0x7f05000c;
+        public static final int error_incorrect_password=0x7f05000b;
+        public static final int error_incorrect_url_format=0x7f05000e;
+        public static final int error_invalid_email=0x7f050009;
+        public static final int error_invalid_password=0x7f05000a;
+        public static final int error_invalid_server_answer=0x7f05000f;
+        public static final int error_unkown_error=0x7f050012;
+        public static final int error_wrong_url=0x7f050010;
         public static final int label=0x7f050001;
-        public static final int login_progress_signing_in=0x7f05000a;
-        public static final int menu_forgot_password=0x7f050009;
-        public static final int pref_default_display_name=0x7f05001a;
-        public static final int pref_description_social_recommendations=0x7f050018;
-        /**  Example settings for Data & Sync 
-         */
-        public static final int pref_header_data_sync=0x7f05001c;
-        /**  Strings related to Settings 
- Example General settings 
-         */
-        public static final int pref_header_general=0x7f050016;
-        /**  Example settings for Notifications 
-         */
-        public static final int pref_header_notifications=0x7f05001f;
-        public static final int pref_invalid_cert=0x7f050003;
-        public static final int pref_protocol_title_fmt=0x7f050002;
-        public static final int pref_ringtone_silent=0x7f050022;
-        public static final int pref_title_add_friends_to_messages=0x7f05001b;
-        public static final int pref_title_display_name=0x7f050019;
-        public static final int pref_title_new_message_notifications=0x7f050020;
-        public static final int pref_title_ringtone=0x7f050021;
-        public static final int pref_title_social_recommendations=0x7f050017;
-        public static final int pref_title_sync_frequency=0x7f05001d;
-        public static final int pref_title_system_sync_settings=0x7f05001e;
-        public static final int pref_title_vibrate=0x7f050023;
-        public static final int prompt_password=0x7f050006;
-        public static final int prompt_url=0x7f05000f;
+        public static final int login_progress_signing_in=0x7f050008;
+        public static final int menu_forgot_password=0x7f050007;
+        public static final int prompt_password=0x7f050004;
+        public static final int prompt_url=0x7f05000d;
         /**  Strings related to login 
          */
-        public static final int prompt_user=0x7f050005;
-        public static final int title_activity_authenticator=0x7f050004;
-        public static final int title_activity_settings=0x7f050015;
+        public static final int prompt_user=0x7f050003;
+        public static final int success_calendar=0x7f050013;
+        public static final int success_collection=0x7f050014;
+        public static final int title_activity_authenticator=0x7f050002;
     }
     public static final class style {
         /** 
@@ -107,20 +78,15 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int LoginFormContainer=0x7f070002;
+        public static final int AppTheme=0x7f060001;
+        public static final int LoginFormContainer=0x7f060002;
     }
     public static final class xml {
-        public static final int account_preferences=0x7f040000;
-        public static final int authenticator=0x7f040001;
-        public static final int pref_data_sync=0x7f040002;
-        public static final int pref_general=0x7f040003;
-        public static final int pref_headers=0x7f040004;
-        public static final int pref_notification=0x7f040005;
-        public static final int syncadapter=0x7f040006;
+        public static final int authenticator=0x7f040000;
+        public static final int syncadapter=0x7f040001;
     }
 }
